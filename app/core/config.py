f67339739e2e696370117246e82da14ade4bbd08
Dharmik Patel <pateldharmik202004@gmail.com>
2026-07-21 11:54:00 +0530

    update

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,9 @@
-import os
+import os 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-print("GOOGLE KEY FOUND:", bool(os.getenv("GOOGLE_API_KEY")))
+GOOGLE_API_KEY  = os.getenv("GOOGLE_API_KEY")
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-  
-if not GOOGLE_API_KEY:
-    raise ValueError("GOOGLE_API_KEY is missing")
+if not GOOGLE_API_KEY  :
+    raise ValueError("GOOGLE_API_KEY is missing from .env")
